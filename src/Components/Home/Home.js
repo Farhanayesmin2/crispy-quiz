3fc466608ad891ddef5581b324f7c0633985d041
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-10-12 23:45:26 +0600

    Header img added

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import './Home.css';
+import logo from "../../Images/images-book-1-5.webp";
 const Home = () => {
     return (
         <div className='container  mx-auto text-center'>
             
             <div className='background '>
                
-                <div className="carousel w-full ">
+          <div className="carousel w-full ">
+        
             <div id="item1" className="carousel-item text-center w-full relative h-48 ">
+            
               <div>
                 <h1 className='absolute inset-0 text-4xl font-bold animate-pulse py-8  '>Get Premium Quiz Set</h1>
                 <span className='absolute inset-0 py-20 '>Gain Your Knowledge</span>
               </div>
+              <img className='w-52 ' src={logo}></img>
               <div className='absolute inset-0 pt-28 '>
             
-              <button className="  btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg animate-pulse ">Register Now</button>
+                <button className="  btn btn-primary btn-xs sm:btn-sm md:btn-md lg:btn-lg animate-pulse ">Register Now</button>
+              
               </div>
             
  
@@ -42,7 +47,7 @@ const Home = () => {
     
   </div> 
   <div id="item3" className="carousel-item w-full">
-   
+   <img className='w-52 ' src={logo}></img>
   </div> 
   <div id="item4" className="carousel-item w-full">
     
