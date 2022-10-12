d9b2ee9f99cbaaa26f2072bf8c19801365300869
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-10-12 12:31:54 +0600

    Toast Added

--- src/Components/AllQuiz/AllQuiz.js
+++ src/Components/AllQuiz/AllQuiz.js
@@ -10,10 +10,13 @@ const AllQuiz = () => {
     return (
         <div>
             <h1>This is all quiz..</h1>
+            <div className='grid gap-6 mx-10'>
             {
                  allQuiz.data.questions.map(topic => <Topic topic={topic}></Topic>)
                
             }
+            </div>
+           
             
              
         </div>
--- src/Components/Topic/Topic.js
+++ src/Components/Topic/Topic.js
@@ -1,38 +1,63 @@
+import { CheckCircleIcon, EyeIcon, QuestionMarkCircleIcon } from '@heroicons/react/24/solid';
 import React from 'react';
 
 
 
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+
+  
+
+
+
+
+
 const Topic = ({ topic }) => {
-    const { question, logo, correctAnswer,option } = topic;
-   console.log(topic.options);
+
+
+
+    const { question, logo, correctAnswer, option } = topic;
+    console.log(topic.options);
  
     const questions = question.split('<p>', 2);
-  
+    const notify = () => toast.success(correctAnswer);
+        
+      
+
     return (
         <div>
             
          
-                <div className="card w-full container mx-auto image-full">
+                <div className="card  w-full container mx-auto ">
   
-  <div className="card-body">
-    <h2 className="card-title"> <h1> {question}</h1> </h2>
-                    <div className='grid grid-cols-2 text-start gap-5'>
+  <div className="card-body bg-gradient-to-r from-cyan-200 to-gray-200">
+    <h2 className="card-title"> <h1><QuestionMarkCircleIcon className=' h-6 w-6 text-purple-900 inline-block'></QuestionMarkCircleIcon> {questions}</h1> </h2>
+                    <div className='grid grid-cols-1 text-start gap-5'>
                            
-          <div>
+          <div className='grid grid-cols-2 text-start gap-5'>
             {
-                topic.options.map(answer => <div  
-                >  <input type="radio" name="radio-4" className="radio radio-accent" checked />{answer}</div>)   
+                topic.options.map(answer => <div className='flex items-center px-12 '
+                >  <input type="radio" name="radio-4" className="radio radio-accent text-xl font-extralight" checked />{answer}</div>)   
         }
             </div>   
-                       
+                     
                    
    </div>
     <div className="card-actions justify-end">
-      <button className="btn btn-success">Buy Now</button>
+                        <button onClick={notify}><EyeIcon className=' h-8 w-8 hover:text-indigo-600 text-purple-300 '></EyeIcon>  </button> 
+                        <ToastContainer 
+                     
+                        autoClose={5000}
+                        className="pt-10  text-center"
+                        
+                        
+                        
+                        />                 
     </div>
   </div>
 </div>
-         
+
         </div>
     );
 };
