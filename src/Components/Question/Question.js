b43a4fd4f1bdff772b403c05aa065349427465fc
Farhanayesmin2 <farhanayesmin@gmail.com>
2022-10-12 20:33:12 +0600

    Question Answer Added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -17,15 +17,32 @@ const Question = () => {
             <div className='pt-12'>
             <div className=" card w-full container mx-auto  bg-base-100 shadow-xl bg-gradient-to-r from-red-200 to-gray-200 ">
   <div className="card-body">
-    <h2 className="card-title "> <QuestionMarkCircleIcon className='w-4 h-4'></QuestionMarkCircleIcon>    What is the purpose of react router?</h2>
-    <p className='text-start'>ReactJS Router is mainly used for developing Single Page Web Applications. React Router is used to define multiple routes in the application. When a user types a specific URL into the browser, and if this URL path matches any 'route' inside the router file, the user will be redirected to that particular route.</p>
+    <h2 className="card-title "> <QuestionMarkCircleIcon className='w-4 h-4'></QuestionMarkCircleIcon> How does work  context api in React?</h2>
+    <p className='text-start'>The React Context API is a way for a React app to effectively produce global variables that can be passed around. This is the alternative to "prop drilling" or moving props from grandparent to child to parent, and so on. Context is also touted as an easier, lighter approach to state management using Redux.
+                        </p>
+    <div className="card-actions justify-end">
+    </div>
+    </div>
+  </div>
+           
+            <div className='py-12'>
+            <div className=" card w-full container mx-auto  bg-base-100 shadow-xl bg-gradient-to-r from-green-200 to-gray-200 ">
+  <div className="card-body">
+    <h2 className="card-title "> <QuestionMarkCircleIcon className='w-4 h-4'></QuestionMarkCircleIcon> How does work  useRef Hook?</h2>
+    <p className='text-start'>
+    useRef() only returns one item. It returns an Object called current.
+When we initialize useRef we set the initial value: useRef(0).
+                                It's like doing this: const count = current: 0. We can access the count by using count.current.
+                                useRef returns a mutable ref object whose .current property is initialized to the passed argument ( initialValue ). The returned object will persist for the full lifetime of the component. Essentially, useRef is like a “box” that can hold a mutable value in its .current property.
+                        </p>
     <div className="card-actions justify-end">
     </div>
     </div>
   </div>
 </div>
             
-        </div>
+            </div>
+            </div>
     );
 };
 
--- src/Components/Statistics/Statistics.js
+++ src/Components/Statistics/Statistics.js
@@ -1,68 +1,16 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer, Bar, BarChart } from 'recharts';
 
 
-const Statistics = ({ chart }) => {
-    
-    const { total, name, id } = chart;
-    // const dataLoad = useLoaderData();
-    console.log(chart);
-
-
-
-    const data = [
-        {
-          name: 'Page A',
-          id: 4000,
-          pv: 2400,
-          amt: 2400,
-        },
-        {
-          name: 'Page B',
-          uv: 3000,
-          pv: 1398,
-          amt: 2210,
-        },
-        {
-          name: 'Page C',
-          uv: 2000,
-          pv: 9800,
-          amt: 2290,
-        },
-        {
-          name: 'Page D',
-          uv: 2780,
-          pv: 3908,
-          amt: 2000,
-        },
-        {
-          name: 'Page E',
-          uv: 1890,
-          pv: 4800,
-          amt: 2181,
-        },
-        {
-          name: 'Page F',
-          uv: 2390,
-          pv: 3800,
-          amt: 2500,
-        },
-        {
-          name: 'Page G',
-          uv: 3490,
-          pv: 4300,
-          amt: 2100,
-        },
-      ];
+const Statistics = () => {
+    const reChart = useLoaderData();
+    const { data } = reChart; 
+        console.log(data);
     return (
-        <div>
-           
+        <div>  
 
-
-
-
-             <LineChart
+            <BarChart
           width={500}
           height={300}
           data={data}
@@ -73,13 +21,14 @@ const Statistics = ({ chart }) => {
             bottom: 5,
           }}
             >
-                 <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey={total} />
-                <YAxis dataKey={ id} />
+                 <CartesianGrid strokeDasharray="2 3" />
+          <XAxis dataKey="name" />
+                <YAxis dataKey="total" />
           <Tooltip />
           <Legend />
-        <Line type="monotone" dataKey={total} stroke="#82ca9d" />
-        </LineChart>
+        <Bar  dataKey="total" stackId="a" stroke="#8884d8" />
+        <Bar  dataKey="" stackId="a" stroke="#82ca9d" />
+        </BarChart>
         </div>
     );
 };
